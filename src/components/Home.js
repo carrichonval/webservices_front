34bcc7a1cc840cfad1605c7f948fa738557c0555
Valentinc <valentin.carrichon@outlook.com>
2020-10-15 14:56:58 +0200

    add

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,45 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 
 
 export default function Home (){
 
+    const [sports,setSports] = useState([])
+
+
+    function fetchAPI(){
+        fetch("https://api.valentincarrichon.fr/sports/",{
+            method:'GET',
+            headers:{
+                "Content-Type":"application/json",
+            },
+        })
+        .then((response)=>{
+            return response.json()
+        })
+        .then((json)=>{
+           console.log(json)
+           setSports(json.sports)
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+
+    useEffect(()=>{
+        fetchAPI()
+    },[])
+
+    console.log("SPORTS",sports)
+
     return (
-        <div>Home</div>
+        <>
+            <div>Home</div>
+            <ul>
+                {sports.map((sport)=>{
+                    return(
+                        <li key={sport.id}>{sport.name}</li>
+                    )
+                })}
+            </ul>
+        </>
     )
 }
