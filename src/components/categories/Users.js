c4a50873a59998d236ecf5c397757a221440390a
valentincarrichon <valentincarrichon@outlook.com>
2021-01-21 12:14:54 +0100

    fix users search

--- src/components/categories/Users.js
+++ src/components/categories/Users.js
@@ -48,7 +48,7 @@ export default function Users (props) {
                 </div>
                 <div className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
                 {users.map((user)=>{
-                    if(user.name.toLowerCase().search(search.toLowerCase()) === -1 ){
+                    if(user.first_name.toLowerCase().search(search.toLowerCase()) === -1 ){
                         return null
                     }
                     return(
